501b2d4a413dff91a450991cdafa33e6f1db780c
Rafa <zoldan.rafael@gmail.com>
2019-11-20 21:37:09 +0200

    Updates

---
+++ precache-manifest.b806c92fca4b3c191b21d1cdced19785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bc79dddaea5ea885b29dd485a7d44d",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "89c4a09935c8d1ab739e",
+    "url": "/instagram-ui/static/css/main.b2180794.chunk.css"
+  },
+  {
+    "revision": "4fd4e13da849d2297e6e",
+    "url": "/instagram-ui/static/js/2.6e7c8600.chunk.js"
+  },
+  {
+    "revision": "89c4a09935c8d1ab739e",
+    "url": "/instagram-ui/static/js/main.1da3a21c.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
