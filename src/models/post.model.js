be06433bc149b872d62a12fc04cf5429b616d75c
Rafa <zoldan.rafael@gmail.com>
2019-11-27 12:09:49 +0200

    changed background to hardcode link, description en form errors added

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 let schema = yup.object().shape({
-    title: yup.string().min(2).max(255).required(),
-    image: yup.mixed().required(),
+    title: yup.string().min(2).max(255).required('Write something about your post'),
+    image: yup.mixed().required('Add an image to your post'),
     tags: yup.string()
 });
 
