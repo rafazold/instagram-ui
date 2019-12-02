b5d4de3e6b3528d811224460136d5e6f95a4f6c4
Rafa <zoldan.rafael@gmail.com>
2019-12-02 16:13:32 +0200

    sync post with server and fixed tags issue in posr create

--- src/CreatePost/CreatePost.js
+++ src/CreatePost/CreatePost.js
@@ -5,51 +5,93 @@ import postModel from '../models/post.model'
 import "./CreatePost.scss"
 import { GoCloudUpload } from "react-icons/go";
 import { FaRegShareSquare } from "react-icons/fa";
-import Tags from "./Tags/Tags";
-
+import config from '../config'
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css'
 
 
 
 class CreatePost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {tags: []}
+    }
+
+    handleTagsChange(tags) {
+            this.setState({
+                tags: tags
+            });
+        }
+
+    convertToFormData(values) {
+        const data = new FormData();
+        for (let key in values) {
+            Array.isArray(values[key])
+                ? values[key].forEach(value => data.append(key + '[]', value))
+                : data.append(key, values[key]) ;
+        }
+        return data;
+    }
 
     submit(values) {
-        console.log(values)
+        console.log('values: ', values);
+        console.log('new values: ', this.convertToFormData(values));
+        const data = new FormData();
+        for (let key in values) {
+            data.append(key, values[key]);
+        }
+        fetch(config.apiUrl + '/api/posts', {
+            method: 'POST',
+            body: data
+        })
+            .then(res => res.json())
+            .then(post => console.log('post:', post))
+            .catch(err => console.log(err));
     }
 
     render() {
         return (
             <div className="post-create">
                 <h2 className="">Create Post</h2>
-                <Formik initialValues={{image: '', title: '', tags: ''}}
+                <Formik initialValues={{image: '', title: '', tags: []}}
                         validationSchema={postModel}
-                        onSubmit={this.submit.bind(this)}>
-                    <Form className="col-xs-12 col-md-6 m-auto">
-                        <div className="row form-group ">
-                            <Field name="image" type="file" id="image"/>
-                            <label htmlFor="image" className="upload-button">
+                        onSubmit={this.submit.bind(this)}
+                        render={({setFieldValue}) => {
+                            return <Form className="col-xs-12 col-md-6 m-auto">
+                                <div className="row form-group ">
+                                    <input name="image" type="file" id="image" onChange={(event => {
+                                        setFieldValue('image', event.currentTarget.files[0]);
+                                    })} />
+                                    <label htmlFor="image" className="upload-button">
                                 <span>
                                     <GoCloudUpload /> Select image
                                 </span>
-                            </label>
-                            <div className="container-fluid row">
-                                <ErrorMessage className="alert alert-danger mt-2" name="image" component="div" />
-                            </div>
-                        </div>
-                        <div className="row form-group">
-                            <label>Title:</label>
-                            <Field name="title" as="textarea" placeholder="About this post..." className="form-control"/>
-                            <ErrorMessage className="alert alert-danger" name="title" component="div" />
-                        </div>
-                        <div className="row form-group">
-                            <label>tags:</label>
-                            <Tags name="tags" />
-                            <div><sub>Press Enter after each a tag</sub></div>
-                        </div>
-                        <div className="row form-group d-flex justify-content-end">
-                            <Button type="submit"><FaRegShareSquare /> Share your post</Button>
-                        </div>
+                                    </label>
+                                    <div className="container-fluid row">
+                                        <ErrorMessage className="alert alert-danger mt-2" name="image" component="div" />
+                                    </div>
+                                </div>
+                                <div className="row form-group">
+                                    <label>Title:</label>
+                                    <Field name="title" as="textarea" placeholder="About this post..." className="form-control"/>
+                                    <ErrorMessage className="alert alert-danger" name="title" component="div" />
+                                </div>
+                                <div className="row form-group">
+                                    <label>Tags:</label>
+                                    <TagsInput value={this.state.tags}
+									           onChange={(tags) => {
+									               this.handleTagsChange(tags);
+										           setFieldValue('tags', tags);
+									           }}
+									           className="tags-wrapper create-post-tags" />
+                                    <div><sub>Press Enter after each a tag</sub></div>
+                                </div>
+                                <div className="row form-group d-flex justify-content-end">
+                                    <Button type="submit"><FaRegShareSquare /> Share your post</Button>
+                                </div>
+                            </Form>
+                        }} >
 
-                    </Form>
                 </Formik>
             </div>
         );
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 let schema = yup.object().shape({
     title: yup.string().min(2).max(255).required('Write something about your post'),
     image: yup.mixed().required('Add an image to your post'),
-    tags: yup.string()
+    tags: yup.array()
 });
 
 export default schema;
