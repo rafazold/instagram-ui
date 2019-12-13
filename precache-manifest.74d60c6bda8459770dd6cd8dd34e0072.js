2b25aa855c3d6bfd959fcfa8c990cca597c4f262
Rafa <zoldan.rafael@gmail.com>
2019-12-13 11:42:25 +0200

    Updates

---
+++ precache-manifest.74d60c6bda8459770dd6cd8dd34e0072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4160f3e37af1469b7c9197863aaf3283",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "f36b1230b2d4a13729bf",
+    "url": "/instagram-ui/static/css/2.06b08db3.chunk.css"
+  },
+  {
+    "revision": "09c4c5662d24dc4d6229",
+    "url": "/instagram-ui/static/css/main.e579a9c7.chunk.css"
+  },
+  {
+    "revision": "f36b1230b2d4a13729bf",
+    "url": "/instagram-ui/static/js/2.b280d472.chunk.js"
+  },
+  {
+    "revision": "09c4c5662d24dc4d6229",
+    "url": "/instagram-ui/static/js/main.4d65a228.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/instagram-ui/precache-manifest.407fd02160f82e99318f1ce1aed332d9.js"
+  "/instagram-ui/precache-manifest.74d60c6bda8459770dd6cd8dd34e0072.js"
 );
 
 self.addEventListener('message', (event) => {
